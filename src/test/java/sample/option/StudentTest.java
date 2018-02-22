c7d237d5edbf6568109ae64f9d6e3f20baf4ddf2
Nattaporn Chatmalairut <nattaporn.cha@ascendcorp.com>
2018-02-22 14:18:50 +0700

    Add more case with Option.

--- src/test/java/sample/option/StudentTest.java
+++ src/test/java/sample/option/StudentTest.java
@@ -1,5 +1,7 @@
 package sample.option;
 
+import io.vavr.control.Option;
+import lombok.Data;
 import lombok.val;
 import org.junit.Test;
 
@@ -10,39 +12,61 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StudentTest {
 
 
-  @Test
-  public void testOptional() {
-    val student = new Student();
-    val address = new Address();
-    Location location = new Location();
-    location.setLocation("Minburi");
-    address.setLocation(Optional.of(location));
-    student.setAddress(Optional.of(address));
-    String locate = retrieveAddress(student)
-            .flatMap(a -> a.getLocation())
-            .flatMap(l -> Optional.of(l.getLocation()))
-            .filter(b -> b.equalsIgnoreCase("Minburi"))
-            .orElse("Not Found");
-    System.out.println(locate);
-    assertThat(locate).isEqualTo("Minburi");
-  }
+    @Test
+    public void testOptional() {
+        val student = new Student();
+        val address = new Address();
+        Location location = new Location();
+        location.setLocation("Minburi");
+        address.setLocation(Optional.of(location));
+        student.setAddress(Optional.of(address));
+        String locate = retrieveAddress(student)
+                .flatMap(a -> a.getLocation())
+                .flatMap(l -> Optional.of(l.getLocation()))
+                .filter(b -> b.equalsIgnoreCase("Minburi"))
+                .orElse("Not Found");
+        System.out.println(locate);
+        assertThat(locate).isEqualTo("Minburi");
+    }
 
-  @Test
-  public void testOptionalNull() {
-    val student = new Student();
+    @Test
+    public void testOptionalNull() {
+        val student = new Student();
 
-    String locate = Optional.ofNullable(student.address)
-            .filter(Optional::isPresent)
-            .flatMap(address -> address)
-            .flatMap(Address::getLocation)
-            .flatMap(l -> Optional.of(l.getLocation()))
-            .filter(b -> b.equalsIgnoreCase("Some Where"))
-            .orElse("Not Found");
-    System.out.println(locate);
-    assertThat(locate).isEqualTo("Not Found");
-  }
+        String locate = Optional.ofNullable(student.address)
+                .filter(Optional::isPresent)
+                .flatMap(address -> address)
+                .flatMap(Address::getLocation)
+                .flatMap(l -> Optional.of(l.getLocation()))
+                .filter(b -> b.equalsIgnoreCase("Some Where"))
+                .orElse("Not Found");
+        System.out.println(locate);
+        assertThat(locate).isEqualTo("Not Found");
+    }
 
-  private Optional<Address> retrieveAddress(Student student) {
-    return student.getAddress();
-  }
+    @Test(expected = CustomExceptoin.class)
+    public void testOptionalThrowFromNull() throws Exception {
+        val student = new SomeStudent();
+        student.setAddress(Option.of(null));
+
+        assertThat(student.getAddress().getOrElseThrow(() -> new CustomExceptoin()).getLocation()).isEqualTo("");
+
+    }
+
+    private Optional<Address> retrieveAddress(Student student) {
+        return student.getAddress();
+    }
+
+    @Data
+    static class SomeStudent {
+        private Option<Address> address;
+    }
+
+    @Data
+    static class SomeAddress {
+        private String location;
+    }
+}
+
+class CustomExceptoin extends Exception {
 }
