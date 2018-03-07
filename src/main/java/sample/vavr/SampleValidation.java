2a0dad988fc46e3d16e38a6a8c61602b9118c3a4
Nattaporn Chatmalairut <nattaporn.cha@ascendcorp.com>
2018-03-07 17:29:33 +0700

    Add test case validation with flatmap, maperror, test combine invalid case.

--- src/main/java/sample/vavr/SampleValidation.java
+++ src/main/java/sample/vavr/SampleValidation.java
@@ -3,7 +3,12 @@ package sample.vavr;
 
 import io.vavr.control.Validation;
 
+import java.util.regex.Pattern;
+
 public class SampleValidation {
+    private static final String EMAIL_PATTERN =
+            "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                    + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
     private SampleValidation() {
     }
@@ -14,5 +19,11 @@ public class SampleValidation {
 
     public static Validation<String, String> validateAddress(final String n) {
         return (n.length() > 1) ? ("test".equalsIgnoreCase(n)) ? Validation.valid("Ok") : Validation.invalid("Wrong Address") : Validation.invalid("Wrong Address");
+    }
+
+    public static Validation<String, String> validateEmail(final String n) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+
+        return (n.length() > 1) ? pattern.matcher(n).matches() ? Validation.valid("Ok") : Validation.invalid("Wrong email") : Validation.invalid("Wrong email");
     }
 }
